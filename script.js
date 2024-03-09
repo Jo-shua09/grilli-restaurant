f206fe5eca9fae846176515b9cefc6fc23a5b67f
jo-shua33 <126611915+jo-shua33@users.noreply.github.com>
2024-03-08 18:30:13 -0800

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // ===========NAVBAR SETTINGS===========
-
 // ================STICKY NAVBAR=================
 const header = document.querySelector(".header-2");
 window.addEventListener("scroll", function () {
@@ -12,14 +11,16 @@ window.addEventListener("scroll", function () {
 // ================STICKY NAVBAR=================
 
 // ===============RESPONSIVE NAVBAR==============
-const navOpen = document.querySelector("#navOpen");
+const navOpen = document.querySelectorAll("#navOpen");
 const closeNav = document.querySelector(".fa-times");
 const navitems = document.querySelector(".nav-items");
 const navBar = document.querySelector(".responsive-navigators");
 
-navOpen.addEventListener("click", () => {
-  navBar.classList.add("show");
-  navitems.classList.add("hide");
+navOpen.forEach(function (element) {
+  element.addEventListener("click", () => {
+    navBar.classList.add("show");
+    navitems.classList.add("hide");
+  });
 });
 closeNav.addEventListener("click", () => {
   navBar.classList.remove("show");
@@ -30,22 +31,6 @@ closeNav.addEventListener("click", () => {
 //===========NAVBAR SETTINGS===========
 
 //=========HOME CONTENT SETTINGS=========
-//=========HOME CONTENT DELAY=========
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   document.querySelector(".contents");
-// });
-//   // const trans1 = document.querySelector(".contents1").classList.add("visible");
-//   // const trans2 = document.querySelector(".contents2").classList.add("visible");
-//   // const trans3 = document.querySelector(".contents3").classList.add("visible");
-// });
-
-// const trans = document.querySelectorAll(".contents");
-// trans.forEach(function (element) {
-//   element.classList.toggle("visible");
-// });
-
-//=========HOME CONTENT DELAY=========
 
 //===========HOME CONTENT SLIDER===========
 var swiper = new Swiper(".mySwiper", {
